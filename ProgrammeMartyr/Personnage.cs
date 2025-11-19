be05f1da75edee6e152691ea02245fc2a508eb75
Solune Petit <210225@site.asty-moulin.be>
2025-11-19 08:51:03 +0100

    2025-11-19_08h50
    ajout d'une méthode pour faire afficher une carte du personnage pour avoir ses infos affichées

--- ProgrammeMartyr/Personnage.cs
+++ ProgrammeMartyr/Personnage.cs
@@ -66,18 +66,40 @@ namespace ProgrammeMartyr
         }
 
         // Méthodes
+
+        //<summary>
+        //Logique pour concevoir la carte du personnage
+        ///Retourne un StackPanel contenant l'image, le nom, le niveau/niveau max, etc.
+        //</summary>
         public StackPanel CardDesign()
         {
-            // Logique pour concevoir la carte du personnage
+            //ajout de l'image
             StackPanel cardPanel = new StackPanel();
+            cardPanel.Width = 150;
+            cardPanel.Height = 400;
             BitmapImage img = new BitmapImage();
             img.BeginInit();
             img.UriSource = new Uri(_img, UriKind.RelativeOrAbsolute);
             img.EndInit();
             Image ppPerso = new Image();
             ppPerso.Source = img;
+            ppPerso.Height = 50;
             cardPanel.Children.Add(ppPerso);
-            //ajotuer d'autres éléments comme le nom, le rôle, etc.
+
+            //ajouter le nom
+            TextBlock txtNom = new TextBlock();
+            txtNom.Text = $"Nom : {_nom}";
+            cardPanel.Children.Add(txtNom);
+
+            //ajotuer le role
+            TextBlock txtRole = new TextBlock();
+            txtRole.Text = $"Genre : {_role}";
+            cardPanel.Children.Add(txtRole);
+
+            //ajouter le niveau/niveau max
+            TextBlock txtNiveau = new TextBlock();
+            txtNiveau.Text = $"Niveau : {_niveauActuel}/{_niveauMax}";
+            cardPanel.Children.Add(txtNiveau);
             return cardPanel;
         }
     }
