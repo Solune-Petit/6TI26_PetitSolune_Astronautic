1a8ce29d883b6a2b7d8272cd4a81917ae111cfc2
AugustinCollin <240400@site.asty-moulin.be>
2026-03-30 17:15:52 +0200

    2026-03-30_17h15
    plateau et images

--- ProgrammeMartyr/Fonctions.cs
+++ ProgrammeMartyr/Fonctions.cs
@@ -399,5 +399,116 @@ namespace ProgrammeMartyr
             NettoyerGrid(grdMain, 10, 10);
             //ajouter les éléments de la fenêtre de jeu ici
         }
+        public void plateau(MainWindow plateau)
+        {
+            MainWindow pagePrincipale = (MainWindow)App.Current.MainWindow;
+
+            //primère ligne 
+            TextBlock texte1 = new TextBlock();
+            texte1.TextAlignment = TextAlignment.Center;
+            texte1.Text = "timer (reste à le créer plus tard)";
+            texte1.FontSize = 15;
+            texte1.FontFamily = new FontFamily("arial");
+            texte1.FontWeight = FontWeights.UltraBold;
+            texte1.Foreground = Brushes.Black;
+            Grid.SetColumn(texte1, 0);
+            Grid.SetRow(texte1, 0);
+            Grid.SetColumnSpan(texte1, 1);
+            Grid.SetColumnSpan(texte1, 5);
+            pagePrincipale.grdMain.Children.Add(texte1);
+
+            //deuxième ligne 
+            TextBlock texte2 = new TextBlock();
+            texte2.Text = "Allié N°1";
+            Grid.SetColumn(texte2, 0);
+            Grid.SetRow(texte2, 1);
+            pagePrincipale.grdMain.Children.Add(texte2);
+
+
+            TextBlock texte3 = new TextBlock();
+            Grid.SetColumn(texte3, 4);
+            Grid.SetRow(texte3, 1);
+            texte3.Text = "Ennemi N°1";
+            texte3.TextAlignment = TextAlignment.Right;
+            pagePrincipale.grdMain.Children.Add(texte3);
+
+            //troisième ligne 
+            TextBlock texte4 = new TextBlock();
+            texte4.Text = "Allié N°2";
+            Grid.SetColumn(texte4, 0);
+            Grid.SetRow(texte4, 2);
+            pagePrincipale.grdMain.Children.Add(texte4);
+
+
+            TextBlock texte5 = new TextBlock();
+            Grid.SetColumn(texte5, 4);
+            Grid.SetRow(texte5, 2);
+            texte5.Text = "Ennemi N°2";
+            texte5.TextAlignment = TextAlignment.Right;
+            pagePrincipale.grdMain.Children.Add(texte5);
+
+
+            TextBlock texte6 = new TextBlock();
+            texte6.Text = "Allié N°3";
+            Grid.SetColumn(texte6, 1);
+            Grid.SetRow(texte6, 2);
+            pagePrincipale.grdMain.Children.Add(texte6);
+
+
+            TextBlock texte7 = new TextBlock();
+            Grid.SetColumn(texte7, 3);
+            Grid.SetRow(texte7, 2);
+            texte7.Text = "Ennemi N°3";
+            texte7.TextAlignment = TextAlignment.Right;
+            pagePrincipale.grdMain.Children.Add(texte7);
+
+            //quatrième ligne 
+            TextBlock texte8 = new TextBlock();
+            texte8.Text = "Allié N°4";
+            Grid.SetColumn(texte8, 0);
+            Grid.SetRow(texte8, 3);
+            pagePrincipale.grdMain.Children.Add(texte8);
+
+
+            TextBlock texte9 = new TextBlock();
+            Grid.SetColumn(texte9, 1);
+            Grid.SetRow(texte9, 3);
+            texte9.Text = "Allié N°5";
+            pagePrincipale.grdMain.Children.Add(texte9);
+
+
+            TextBlock texte10 = new TextBlock();
+            texte10.Text = "Ennemi N°4";
+            Grid.SetColumn(texte10, 4);
+            Grid.SetRow(texte10, 3);
+            texte10.TextAlignment = TextAlignment.Right;
+            pagePrincipale.grdMain.Children.Add(texte10);
+
+
+            TextBlock texte11 = new TextBlock();
+            Grid.SetColumn(texte11, 3);
+            Grid.SetRow(texte11, 3);
+            texte11.Text = "Ennemi N°5";
+            texte11.TextAlignment = TextAlignment.Right;
+            pagePrincipale.grdMain.Children.Add(texte11);
+
+            //cinquième ligne 
+            TextBlock texte12 = new TextBlock();
+            texte12.Text = "Buffs et DeBuffs";
+            Grid.SetColumn(texte12, 0);
+            Grid.SetRow(texte12, 4);
+            pagePrincipale.grdMain.Children.Add(texte12);
+
+
+            TextBlock texte13 = new TextBlock();
+            texte13.Text = "Attaque";
+            Grid.SetColumn(texte13, 4);
+            Grid.SetRow(texte13, 4);
+            texte13.FontFamily = new FontFamily("arial");
+            texte13.FontWeight = FontWeights.UltraBold;
+            texte13.Foreground = Brushes.Black;
+            texte13.TextAlignment = TextAlignment.Right;
+            pagePrincipale.grdMain.Children.Add(texte13);
+        }
     }
 }
--- ProgrammeMartyr/obj/Debug/net8.0-windows/ProgrammeMartyr.AssemblyInfo.cs
+++ ProgrammeMartyr/obj/Debug/net8.0-windows/ProgrammeMartyr.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProgrammeMartyr")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d452dcc593f1ae76137568a3b9455d9d5110b1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4da43978ccdca56df0460bbda678356abd18dc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProgrammeMartyr")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProgrammeMartyr")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
