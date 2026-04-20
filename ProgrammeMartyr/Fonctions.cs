26929174b369dc199c7b1b1160a9b005953b6cf4
AugustinCollin <240400@site.asty-moulin.be>
2026-04-20 10:47:54 +0200

    fgdh

--- ProgrammeMartyr/Fonctions.cs
+++ ProgrammeMartyr/Fonctions.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace ProgrammeMartyr
 {
@@ -529,5 +530,14 @@ namespace ProgrammeMartyr
             texte13.TextAlignment = TextAlignment.Right;
             pagePrincipale.grdMain.Children.Add(texte13);
         }
+
+
+        
+       
     }
 }
+
+
+
+
+
--- ProgrammeMartyr/obj/Debug/net8.0-windows/ProgrammeMartyr.AssemblyInfo.cs
+++ ProgrammeMartyr/obj/Debug/net8.0-windows/ProgrammeMartyr.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProgrammeMartyr")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4da43978ccdca56df0460bbda678356abd18dc4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a8ce29d883b6a2b7d8272cd4a81917ae111cfc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProgrammeMartyr")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProgrammeMartyr")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
